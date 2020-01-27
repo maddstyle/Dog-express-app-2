ddd85ebbf04432113d4cb5be017a207e88b9c62e
Michaela DeCamara <michaela.decamara@gmail.com>
2020-01-27 00:00:47 -0500

    changes

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const express = require("express");
 const app = express();
 
 // if u type route "/" response will be "HI!"
-app.get("/", (req, res) => app.send("Hi, there!"));
+app.get("/", function (req, res) {
+    app.send("Hi, there!")
+});
 // if u type route "/bye" response will be "BYE!"
-app.get("/bye", (req, res) => app.send("Goodbye!"));
+app.get("/bye", function (req, res) {
+    app.send("Goodbye!")
+});
 // if u type route "/dog" response will be "WOOF!"
-app.get("/dog", (req, res) => app.send("Woof!"));
+app.get("/dog", function (req, res) {
+    app.send("Woof!")
+});
 
 app.listen(3000, () => {
-console.log("Running on port: 3000")
+    console.log("Running on port: 3000")
 });
